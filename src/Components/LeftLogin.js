43fd8cb1a2df153dcd54e891fe82f699d438d2f0
Reddy_Jayasree <reddyjayasreevalasa@gmail.com>
2024-12-04 22:19:13 +0530

    Updates

--- src/Components/LeftLogin.js
+++
@@ -1,7 +0,0 @@
- export default function LeftLogin(){
-    
-    return <div className="left-login">
-            <img src="https://i.pinimg.com/736x/5d/73/ea/5d73eaabb25e3805de1f8cdea7df4a42--tumblr-backgrounds-iphone-phone-wallpapers-iphone-wallaper-tumblr.jpg"  />
-            <h1 className="glow">Welcome !!</h1>
-    </div>
- }
--- src/Store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productDataReducer from "../Reducer/productDataSlice"
-
-export const store = configureStore({
-  reducer: {
-    productData: productDataReducer,
-  },
-});
